2de27ea485cf863054c258844a0a05efc2edc2a8
Siting <teamillionton@me.com>
2014-05-12 00:06:30 +0800

    plot assignment

---
+++ plot1.R
@@ -0,0 +1,16 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
+dates <- data[,1]
+times <- data[,2]
+x <- paste(dates, times)
+DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+data[["DateTime"]] <- DateTime
+library(lubridate)
+date1 <- as.POSIXct("2007-02-01 00:00:00")
+date2 <- as.POSIXct("2007-02-02 24:00:00")
+int <- new_interval(date1, date2)
+newData <- data[data$DateTime %within% int,]
+png(file = "plot1.png")
+hist(as.numeric(newData[,3]),  col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)",xaxt = 'n')
+axis(side=1, at=seq(0,3000,1000), labels=seq(0,6,2))
+dev.off()
+
